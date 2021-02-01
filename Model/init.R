0d25572eba91a7d4a4fee10dc88249cd3ed50d9e
Srinithyee S K <sksrinithyee@gmail.com>
2021-02-01 16:09:10 +0530

    init wtv

---
+++ Model/init.R
@@ -0,0 +1,8 @@
+helpers.installPackages("shiny", "tidyverse","nortest","mvnormtest","MASS","shinyLP","gmodels","caret","rattle","ranger","kernlab","nnet","mclust","keras","mlbench","randomForest","Metrics","BBmisc","corrplot",
+"lars","xgboost","Matrix","methods","mlr","scales","shinythemes","plotly","data.table","flexdashboard","DiagrammeR")
+
+
+
+
+
+
